a7d279ae85955ef6277709a7b75cb104d89c5925
Kostya Yelskyi <koortttt@gmail.com>
2022-09-05 00:37:11 +0300

    added modal window (added option with animation)

--- src/App.js
+++ src/App.js
@@ -1,32 +1,31 @@
 import React, {useState} from 'react';
 import './index.scss';
 
+const Modal = ({open, setOpen, modalContent}) => (
+    <div className={`overlay animated${open ? ' show' : ''}`}>
+        <div className="modal">
+            <svg onClick={() => setOpen(false)} height="200" viewBox="0 0 200 200" width="200">
+                <title/>
+                <path
+                    d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
+            </svg>
+            <div>{modalContent}</div>
+            <img alt='image' src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"/>
+        </div>
+    </div>
+)
+
 function App() {
-    const [open, isOpen] = useState(false);
-
-    const openModal = () => {
-        isOpen(true);
-    }
-
-    const hideModal = () => {
-        isOpen(false);
-    }
+    const [open, setOpen] = useState(false);
 
     return (
         <div className="App">
-            <button className="open-modal-btn" onClick={openModal}>✨ Открыть окно</button>
-            {open && (
-                <div className="overlay">
-                    <div className="modal">
-                        <svg onClick={hideModal} height="200" viewBox="0 0 200 200" width="200">
-                            <title/>
-                            <path
-                                d="M114,100l49-49a9.9,9.9,0,0,0-14-14L100,86,51,37A9.9,9.9,0,0,0,37,51l49,49L37,149a9.9,9.9,0,0,0,14,14l49-49,49,49a9.9,9.9,0,0,0,14-14Z"/>
-                        </svg>
-                        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif"/>
-                    </div>
-                </div>
-            )}
+            <button className="open-modal-btn" onClick={() => setOpen(true)}>✨ Открыть окно</button>
+            {/* условный рендеринг (без анимации) */}
+            {open && <Modal open={open} setOpen={setOpen} modalContent='i am content inside modal :)'/>}
+
+            {/* модуль грузится в DOM сразу же и благодаря этому будет применена анимация  */}
+            {/*<Modal open={open} setOpen={setOpen} modalContent='i am content inside modal :)'/>*/}
         </div>
     );
 }
