dbc557a05120381dc4a353c824a2df2720326a10
Kostya Yelskyi <koortttt@gmail.com>
2022-10-08 21:56:18 +0300

    photos collection app with mockAPI

--- src/App.js
+++ src/App.js
@@ -1,52 +1,95 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import {Preloader} from "./Preloader";
+import {Collection} from './Collection'
 import './index.scss';
 
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+const CATEGORIES = [
+    {"name": "Все"},
+    {"name": "Море"},
+    {"name": "Горы"},
+    {"name": "Архитектура"},
+    {"name": "Города"}
+];
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Моя коллекция фотографий</h1>
-      <div className="top">
-        <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
-        </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
-      </div>
-      <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
-        />
-      </div>
-      <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
-      </ul>
-    </div>
-  );
+    const [isLoading, setIsLoading] = useState(true);
+    const [page, setPage] = useState(1);
+    const [categoryId, setCategoryId] = useState(0);
+    const [searchValue, setSearchValue] = useState('');
+    const [collections, setCollections] = useState({});
+    const viewLimitPerPage = 3;
+
+    useEffect(() => {
+        setIsLoading(true);
+        const category = categoryId ? `&category=${categoryId}` : '';
+
+        fetch(`https://6340157cd1fcddf69cb0cc67.mockapi.io/photo_collections?${category}&limit=${viewLimitPerPage}&page=${page}`)
+            .then((res) => res.json())
+            .then((json) => {
+                setCollections(json);
+            })
+            .catch((err) => {
+                console.log(err);
+                alert('error');
+            })
+            .finally(() => setIsLoading(false));
+    }, [categoryId, page])
+
+    // why 3 console logs?
+    // why 3 console logs?
+    // why 3 console logs?
+    console.log(collections);
+
+    return (
+        <div className="App">
+            <h1>Моя коллекция фотографий</h1>
+            {
+                isLoading ? <Preloader/> : (
+                    <>
+                        <div className="top">
+                            <ul className="tags">
+                                {
+                                    CATEGORIES.length > 0 && CATEGORIES.map((category, i) =>
+                                        <li className={categoryId === i ? 'active' : ''}
+                                            onClick={() => setCategoryId(i)}
+                                            key={category.name}>
+                                            {category.name}
+                                        </li>)
+                                }
+                            </ul>
+                            <input
+                                className="search-input"
+                                value={searchValue}
+                                onChange={((e) => setSearchValue(e.target.value))}
+                                placeholder="Поиск по названию"/>
+                        </div>
+                        <div className="content">
+                            {
+                                collections?.length > 0 && collections.filter((obj) =>
+                                    obj.name.toLowerCase().includes(searchValue.toLowerCase())
+                                ).map((obj, index) => (
+                                    <Collection
+                                        key={index}
+                                        name={obj.name}
+                                        images={obj.photos}
+                                    />
+                                ))
+                            }
+                        </div>
+                        <ul className="pagination">
+                            {
+                                [...Array(5)].map((_, i) => (
+                                    <li key={i} onClick={() => setPage(i + 1)}
+                                        className={page === i + 1 ? 'active' : ''}>{i + 1}</li>
+                                ))
+                            }
+                        </ul>
+                    </>
+                )
+            }
+
+        </div>
+    );
 }
 
 export default App;
---
+++ src/Preloader.jsx
@@ -0,0 +1,5 @@
+export const Preloader = () => {
+    return (
+        <div className="loading">content is loading... wait a bit</div>
+    )
+}
